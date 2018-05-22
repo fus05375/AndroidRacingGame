71f657635bf6535b6ea2d66c5b70c2e2898568c1
fus05375 <fus05375@gmail.com>
2018-05-22 10:16:36 +0200

    simple 2d view + simplest functionality
    adding player's car, randomly spawniing enemy cars

---
+++ Assets/scripts/carController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carController : MonoBehaviour {
+
+    public float carSpeed;
+    float xMaxPos = 2.0f;
+    float yMaxPos = 3.5f;
+
+    Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+        position = transform.position;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime; // moving horizontal
+        position.x = Mathf.Clamp(position.x, -xMaxPos, xMaxPos); // restricting car not to go beyond map layer
+        position.y += Input.GetAxis("Vertical") * carSpeed * Time.deltaTime;
+        position.y = Mathf.Clamp(position.y, -yMaxPos, yMaxPos);
+
+
+        transform.position = position;
+
+
+    }
+}
---
+++ Assets/scripts/carSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carSpawner : MonoBehaviour {
+
+
+    public GameObject car;
+
+    float maxPos = 2.2f;
+
+    float timer;
+    public float delayTimer = 2f;
+	// Use this for initialization
+	void Start () {
+        timer = delayTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer <= 0) {
+            //putting enemy car randomly
+            Vector3 carPos = new Vector3(Random.Range(-maxPos, maxPos), transform.position.y, transform.position.z);
+            //initation of object
+            Instantiate(car, carPos, transform.rotation);
+            timer = delayTimer;
+        }
+        
+    }
+}
